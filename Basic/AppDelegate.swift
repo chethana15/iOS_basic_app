d17492c42121ebd395da91d3610095371620d8b7
Chethana N P <chethana.np@cumulations.com>
2022-11-20 03:10:16 +0530

    app life cycle

--- Basic/AppDelegate.swift
+++ Basic/AppDelegate.swift
@@ -10,10 +10,14 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("willFinishLaunchingWithOptions")
+        return true
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("didFinishLaunchingWithOptions")
         return true
     }
 
