76a809b885175ac6656ac40830a319799896392f
Chethana N P <chethana.np@cumulations.com>
2022-11-19 11:49:36 +0530

    Initial Commit

---
+++ Basic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Basic
+//
+//  Created by Cumulations Technologies Private Limited on 19/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
